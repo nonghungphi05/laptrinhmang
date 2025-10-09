7b1beac6444045c7c28d89ace6a3d3d8a9f94dce
Cao Xuân Dự <pherocaodu@gmail.com>
2025-10-09 20:28:49 +0700

    create main

---
+++ Main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello World!")
+    
+    # Tính toán cơ bản
+    x = 5
+    y = 3
+    print("x =", x)
+    print("y =", y)
+    print("x + y =", x + y)
+    print("x * y =", x * y)
+
+main()
